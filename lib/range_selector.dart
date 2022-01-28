ac35aed611a3d0d6b80e6c8031a76f7b93015b4c
kartik <kartiksaini8881@gmail.com>
2022-01-28 20:22:57 +0530

    form

--- lib/range_selector.dart
+++ lib/range_selector.dart
@@ -17,6 +17,9 @@ class RangeSelector extends StatefulWidget {
 }
 
 class _RangeSelectorState extends State<RangeSelector> {
+  int _min = 0;
+  int _max = 0;
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -30,22 +33,25 @@ class _RangeSelectorState extends State<RangeSelector> {
       appBar: AppBar(
         title: Text('Randomizer'),
       ),
-      body: Form(child: Padding(
+      body: Form(
+        
+          child: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:[
-            RandomTextFormFieldWidget(labelText: 'Minimum',),
-            SizedBox(height: 8),
-          RandomTextFormFieldWidget(labelText: 'Maximum')
-          ]
-        ),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          RandomTextFormFieldWidget(
+            labelText: 'Minimum',
+            intValueSetter: (value) => _min = value,
+          ),
+          SizedBox(height: 8),
+          RandomTextFormFieldWidget(
+            labelText: 'Maximum',
+            intValueSetter: (value) => _max = value,
+          )
+        ]),
       )),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_forward),
-        onPressed: (){
-            
-        },
+        onPressed: () {},
       ),
     );
   }
@@ -53,15 +59,28 @@ class _RangeSelectorState extends State<RangeSelector> {
 
 class RandomTextFormFieldWidget extends StatelessWidget {
   const RandomTextFormFieldWidget({
-   required this.labelText,
+    required this.labelText,
+    required this.intValueSetter,
     Key? key,
   }) : super(key: key);
+
+  final void Function(int value) intValueSetter;
+
   final String labelText;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      onSaved: (newValue) => intValueSetter(int.parse(newValue ?? '0')),
       keyboardType: TextInputType.number,
-      decoration: InputDecoration(border:OutlineInputBorder(),labelText: this.labelText ),
+      validator: (value) {
+        if ( value == null || int.tryParse(value) == null) {
+          return 'Value must be Integer';
+        } else {
+          return null;
+        }
+      },
+      decoration: InputDecoration(
+          border: OutlineInputBorder(), labelText: this.labelText),
     );
   }
 }
